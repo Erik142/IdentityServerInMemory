c56ce7e62d651269e234c03dd1e54cb5234be5af
Erik Wahlberger <EWahlberger@gmail.com>
2022-03-18 15:08:32 +0100

    Add CompanyUser

--- IdentityServerInMemory/User/CompanyUser.cs
+++ IdentityServerInMemory/User/CompanyUser.cs
@@ -1,6 +1,80 @@
-﻿namespace IdentityServerInMemory.User
+﻿using IdentityModel;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityServerInMemory.User
 {
+    // Copy pasted from TestUser to prove that inheritance is not necessary
     public class CompanyUser
     {
+        public string Company 
+        { 
+            get; 
+            set; 
+        }
+        //
+        // Summary:
+        //     Gets or sets the subject identifier.
+        public string SubjectId
+        {
+            get;
+            set;
+        }
+
+        //
+        // Summary:
+        //     Gets or sets the username.
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        //
+        // Summary:
+        //     Gets or sets the password.
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        //
+        // Summary:
+        //     Gets or sets the provider name.
+        public string ProviderName
+        {
+            get;
+            set;
+        }
+
+        //
+        // Summary:
+        //     Gets or sets the provider subject identifier.
+        public string ProviderSubjectId
+        {
+            get;
+            set;
+        }
+
+        //
+        // Summary:
+        //     Gets or sets if the user is active.
+        public bool IsActive
+        {
+            get;
+            set;
+        } = true;
+
+
+        //
+        // Summary:
+        //     Gets or sets the claims.
+        public ICollection<Claim> Claims
+        {
+            get;
+            set;
+        } = new HashSet<Claim>(new ClaimComparer());
+
     }
 }
